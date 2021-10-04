0e780d6d46ae38de56b363b5bdd859e98ae9dfb9
praddyumnwadekar <wadekarpraddyumn1@gmail.com>
2021-10-04 22:29:10 +0530

    added solutions for 896

---
+++ Monotonic_Array_896.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isMonotonic(int[] nums) {
+        if(nums.length < 2){
+            return true;
+        }
+        
+        int temp = 0;
+        
+        for(int i = 0; i<nums.length-1; i++){
+            if(temp == 0){
+                if(nums[i] < nums[i+1]){
+                    temp = 1;
+                }else if(nums[i] > nums[i+1]){
+                    temp = -1;
+                }
+            }    
+            
+            if(temp == 1){
+                if(nums[i] > nums[i+1]){
+                        return false;
+                }
+            }else if(temp == -1){
+                if(nums[i] < nums[i+1]){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
